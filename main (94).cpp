576c9e43e49f8bdd914fe8f7c0c782b384db219e
Aman-kr477 <129995427+Aman-kr477@users.noreply.github.com>
2024-07-30 22:01:45 +0530

    Add files via upload

---
+++ main (94).cpp
@@ -0,0 +1,58 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+int main()
+{
+    ll t;
+    cin>>t;
+    // we create power vector whih stores 10,100,1000,....10^18;
+    vector<ll>powers(19,1);
+    for(int i=1;i<19;i++){
+        powers[i]=powers[i-1]*10;
+    }
+    while(t--){
+        ll n;
+        cin>>n;
+        ll numdigit=0;
+        ll digitlength=0;
+        ll prevdigit=0;
+        for(ll i=1;i<19;i++){
+            digitlength+=(powers[i]-powers[i-1])*i;
+            if(digitlength>=n){
+                numdigit=i;
+                break;
+            }
+            prevdigit=digitlength;
+        }
+        ll low=powers[numdigit-1];
+        ll high=powers[numdigit]-1;
+        ll ans=0;
+        ll startposAns=0;
+        while(low<=high){
+            ll mid=(high+low)/2;
+            ll startpos=(mid-powers[numdigit-1])*numdigit+prevdigit+1;
+            if(startpos<=n){
+                if(mid>ans){
+                    ans=mid;
+                    startposAns=startpos;
+                }
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        string number=to_string(ans);
+        cout<<number[n-startposAns]<<endl;
+    }
+
+    return 0;
+}
